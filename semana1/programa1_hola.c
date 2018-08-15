4bf01abf948804032171ff2016e4f3a0d0356c43
Segura Gomez Guillermo <seguragg2018@dci25-175.ugto.mx>
2018-08-15 09:43:16 -0500

    git commit . git push .

---
+++ semana1/programa1_hola.c
@@ -0,0 +1,17 @@
+/*Este es un comentario
+Programa creado el 15/08/2018 by Guillermo Segura Gomez
+*/
+
+#include<stdio.h>
+
+int main()
+{
+
+printf("Hola memo \n");
+printf("Este es el primer programa del curso\n ");
+
+}
+
+
+/*Termine de escribir el programa*/
+
