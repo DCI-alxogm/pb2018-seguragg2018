7fb1fb19d9bd2483b7331c3bf033c2b98f12ebf0
seguragg2018 <seguragg2018@licifug.ugto.mx>
2018-09-13 11:18:30 -0500

    Termine el ejercicio 2, subo la base de datos y los resultados

--- semana6/ejercicio2.c
+++ semana6/ejercicio2.c
@@ -2,39 +2,34 @@
 
 int main(){
 
-FILE *archivo;
-fp=open("base.txt", "r");
+FILE *lec;
+lec = fopen("base.txt", "r");
+//Abre un archivo llamado base.txt, y se declara una variable llamada lec, para leer los archivos de la base
 
-//Primero se declaran las variables
-int e, i, h, x=0; //Se usan estas para los contadores, y los resultados finales
-int un=0, os=0, tre=0, cua=0, cin=0, six=0, sev=0, oc=0, ni=0; //Estas se usan para determinar de que semestre son los alumnos
-float p, d; //Se usan dos de tipo float para determinar la edad y el promedio
-printf("¡Hola mundo!, este es un programa que solicita informacion sobre los estudiantes\nIntroduce el numero de estudiantes\n");
+int i, h, x=0, n; //Se declaran los contadores enteros
 
-//Se escanea el numero de estudiantes
-scanf("%i", &e);
-int edad[e], sexo[e], sem[e];
-float prom[e];
-//Se declaran los arreglos, tres de tipo entero y uno flotante para los promedios
+//Se lee la primer linea del archivo, que te dice cual es el numero de alumnos de la base
+fscanf(lec, "%i", &n);
 
-printf("Introduce ahora la edad, el sexo, (0)hombre, (1)mujer, semestre (1-9) y promedio de la carrera\n");
+//Se declaran las variables
+int un=0, os=0, tre=0, cua=0, cin=0, six=0, sev=0, oc=0, ni=0; //Estas se usan para determinar el semestre en el que se esta
+float p=0, d=0; //Estas se usan para determinar el promedio y la edad promedio
+int edad[n], sexo[n], sem[n];
+float prom[n];
+//Se declara un arrreglo para cada tipo de dato
 
-//Se hace un ciclo for, y se piden los valores y se guardan en el lugar i de cada arreglo}
-fp=open("base.txt", "r");
-for(i=0; i<e; i++){
-fscanf(archivo, "%i %i %i %f", d, x, h, p);
 
-d  = d; 
+//Se usa un for, se leen los datos y se van guardando en arreglos
+for(i=0; i<n; i++){
+fscanf(lec, "%i %i %i %f", &edad[i], &sexo[i], &sem[i], &prom[i]);
+
+d = edad[i]+d; 
 //Se guarda la edad en edad[i] y se usa d, para sumar el total de las edades
 
-
-printf("Sexo: \n");
-scanf("%i",&sexo[i]);
 x = x + sexo[i];
 //Para calcular el sexo, se usa 1 para las mujeres y 0 para los hombres, usamos la variable x para sumar todos los valores de x, y despues se le restan al total de estudiantes donde x = total de mujeres y e - x = total de hombres
 
-printf("Semestre: \n");
-scanf("%i",&sem[i]);
+
 if (sem[i]==1) un = un + 1;
 if (sem[i]==2) os = os + 1;
 if (sem[i]==3) tre = tre + 1;
@@ -47,22 +42,29 @@ if (sem[i]==9) ni = ni + 1;
 //Se hacen nueve if, y nueve variables, si el numero ingresado es igual a alguno de los semestres, se le suma uno a su respecticva variable
 
 
-printf("Promedio: \n");
-scanf("%f",&prom[i]);
 p = p + prom[i];
 //El promedio suma todos los valores ingresados en una variable p
 }
 
-h = e - x; //Calculo del numero de hombres
-d = d/e; //Calculo de la edad promedio
-p = p/e; //Calculo del promedio
+fclose(lec); //Aqui termina la parte de lectura del archivo, las variables ya tienen valores ahora solo se tienen que hacer calculos e imprimir los resultados
 
-printf("Se capturaron %i alumnos de los cuales %i son mujeres y %i son hombres\n", e, x, h);
-printf("La edad promedio de los alumnos es %f\n", d);
-printf("El numero de estudiantes por semestre es: \n Primero: %i\n Segundo: %i\n Tercero: %i\n Cuarto: %i\n Quinto: %i\n Sexto: %i\n Septimo: %i\n Octavo: %i\n Noveno: %i\n", un, os, tre, cua, cin, six, sev, oc, ni);
-printf("El promedio de calificaciones de todos los estudiantes es: %f\n", p);
+h = n - x; //Calculo del numero de hombres
+d = d/n; //Calculo de la edad promedio
+p = p/n; //Calculo del promedio
 
-//Finalmente solo se imprimen los resultados finales de todo y ya
+//Se declara una nueva variable para escribir
+FILE *re;
+re = fopen("resultados.txt", "w"); //Se escribe en un archivo llamado resultados.txt
+
+//Se escriben los resultados utilizando fprintf
+
+fprintf(re, "Se capturaron %i alumnos de los cuales %i son mujeres y %i son hombres\n", n, x, h);
+fprintf(re, "La edad promedio de los alumnos es %f\n", d);
+fprintf(re, "El numero de estudiantes por semestre es: \n Primero: %i\n Segundo: %i\n Tercero: %i\n Cuarto: %i\n Quinto: %i\n Sexto: %i\n Septimo: %i\n Octavo: %i\n Noveno: %i\n", un, os, tre, cua, cin, six, sev, oc, ni);
+fprintf(re, "El promedio de calificaciones de todos los estudiantes es: %f\n", p);
+
+fclose(re);
+//Finalmente se cierra la parte de escritura del archivo y termina el programa
 
 
 return 0;
