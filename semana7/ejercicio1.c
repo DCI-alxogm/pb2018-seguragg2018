c188ee5b89a8ab68ab20cc1fb1d7d1966d7948a0
Segura Gomez Guillermo <seguragg2018@dci25-158.ugto.mx>
2018-09-19 09:58:03 -0500

    Ejercicio 1, semana 7

--- semana7/ejercicio1.c
+++ semana7/ejercicio1.c
@@ -3,31 +3,32 @@
 
 int main(){
 
-int es, p, b, n, k, r, a;
-float integral, mul= 0, h;
+int es, p, b, n, x, a;
+float integral, mul= 0, h=0, k;
 FILE *var;
 var=fopen("base.txt","r");
-fscanf(var, "%i %i %i %i %i", &p, &a, &b, &n, &k);
-es = ((b - (a-1))/n);
+fscanf(var, "%i %i %i %i %f", &p, &a, &b, &n, &k);
+es = (b - (a-1))/n;
 
 
 fclose(var);
 
-FILE *rar;
+FILE *rar; 
 rar = fopen("resultados.txt", "w");
 
-for(int i=0;i<b;i++){
-r = pow (a, p);
+for(int i=a;i<=b;i=i+es){
+x = pow (i, p);
 
-h = ((r-a)/k);
+h = (i-a)/(k);
+
 	for (int j= 1; j<k; j++){
 	mul = mul + pow((a+k*h),p);
 	
 	}
 
-integral = h * (((a+r)/2)+mul);
-fprintf(rar, "%i elevado a la %i es igual a %i, y la integral es: %f\n",a, p, r, integral);
-a= a+es;
+integral = h * (((pow(a, p)+ pow (i, p))/2)+mul);
+fprintf(rar, "%i elevado a la %i es igual a %i, y la integral es: %f\n",i, p, x, integral);
+
 
 }
 fclose(rar);
