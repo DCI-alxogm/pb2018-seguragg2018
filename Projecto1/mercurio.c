8802da71b823e21fcd6216b46e0c39fe400f9053
Segura Gomez Guillermo <seguragg2018@dci25-158.ugto.mx>
2018-09-24 09:53:40 -0500

    master

---
+++ Projecto1/mercurio.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+double x, y, z, vx, vy, vz;
+float h=.0001;
+
+FILE *leer;
+leer=fopen("base.txt", "r");
+fscanf(leer, "%d %d %d %d %d %d", &x, &y, &z, &vx, &vy, &vz);
+fclose(leer);
+r = sqrt(pow(x,2)+pow(y,2)+pow(z,2));
+
+for (int i=0; ){
+
+
+
+
+}
+
+
+
+
+}
