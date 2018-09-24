f8b7d26461a7ca6909b74ad5610b2f840d32951e
Segura Gomez Guillermo <seguragg2018@dci25-188.ugto.mx>
2018-09-24 11:12:27 -0500

    master

--- Projecto1/mercurio.c
+++ Projecto1/mercurio.c
@@ -11,13 +11,27 @@ fscanf(leer, "%d %d %d %d %d %d", &x, &y, &z, &vx, &vy, &vz);
 fclose(leer);
 r = sqrt(pow(x,2)+pow(y,2)+pow(z,2));
 
-for (int i=0; ){
+FILE *esc;
+esc = fopen("mercurio.dat", "w");
 
+fprintf(esc, "Posicion         Velocidad\n");
+fprintf(esc, "%d %d %d %d %d %d", x, y, z, vx, vy, vz);
 
+for (int i=0;i<10;i++){
+x = x + vx * h;
+y = y + vy * h;
+z = z + vz * h; 
 
+vx = vx - h * ((4*3.1416*x)/(pow(r, 3)));
+vy = vy - h * ((4*3.1416*y)/(pow(r, 3)));
+vz = vz - h * ((4*3.1416*z)/(pow(r, 3)));
+
+fprintf(esc, "Posicion         Velocidad\n");
+fprintf(esc, "%d %d %d %d %d %d\n", x, y, z, vx, vy, vz);
 
 }
-
+fclose(esc);
+return 0;
 
 
 
