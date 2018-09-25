dde0289283d485c220ebc868650423cf9b8ca16f
seguragg2018 <seguragg2018@licifug.ugto.mx>
2018-09-25 00:34:07 +0000

    Avance proyecto

--- Projecto1/mercurio.c
+++ Projecto1/mercurio.c
@@ -2,22 +2,23 @@
 #include<math.h>
 
 int main(){
-double x, y, z, vx, vy, vz;
+double r, x, y, z, vx, vy, vz;
 float h=.0001;
 
 FILE *leer;
 leer=fopen("base.txt", "r");
-fscanf(leer, "%d %d %d %d %d %d", &x, &y, &z, &vx, &vy, &vz);
+fscanf(leer, "%lf %lf %lf %lf %lf %lf", &x, &y, &z, &vx, &vy, &vz);
 fclose(leer);
-r = sqrt(pow(x,2)+pow(y,2)+pow(z,2));
+
 
 FILE *esc;
-esc = fopen("mercurio.dat", "w");
+esc = fopen("mercurio.txt", "w");
 
-fprintf(esc, "Posicion         Velocidad\n");
-fprintf(esc, "%d %d %d %d %d %d", x, y, z, vx, vy, vz);
+fprintf(esc, "Posicion                     Velocidad\n");
+fprintf(esc, "%f %f %f %f %f %f\n", x, y, z, vx, vy, vz);
 
 for (int i=0;i<10;i++){
+r = sqrt(pow(x,2)+pow(y,2)+pow(z,2));
 x = x + vx * h;
 y = y + vy * h;
 z = z + vz * h; 
@@ -26,13 +27,13 @@ vx = vx - h * ((4*3.1416*x)/(pow(r, 3)));
 vy = vy - h * ((4*3.1416*y)/(pow(r, 3)));
 vz = vz - h * ((4*3.1416*z)/(pow(r, 3)));
 
-fprintf(esc, "Posicion         Velocidad\n");
-fprintf(esc, "%d %d %d %d %d %d\n", x, y, z, vx, vy, vz);
+fprintf(esc, "%f %f %f %f %f %f\n", x, y, z, vx, vy, vz);
 
 }
 fclose(esc);
+
 return 0;
 
-
-
 }
+
+
