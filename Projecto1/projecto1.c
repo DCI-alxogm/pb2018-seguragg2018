2bf19f1731e521eeb48e270c9e5ba29f097ff958
seguragg2018 <seguragg2018@licifug.ugto.mx>
2018-09-29 17:33:01 +0000

    master

--- Projecto1/projecto1.c
+++ Projecto1/projecto1.c
@@ -1,30 +1,52 @@
+//Programa creado por Guillermo Segura Gomez
+//Editado el 29/09/2018
 #include<stdio.h>
 #include<math.h>
 
 int main(){
-double r, x, y, z, vx, vy, vz, me, t, h, j=0.0;
+//Primero se declaran las variables
+
+//Se usan de tipo double para las posiciones y las velocidades de los planetas, tambien se usan para la masa del planeta, la masa de la estrella, el tiempo total y la evolucion del tiempo
+double r, x, y, z, vx, vy, vz, mp, me, t, h; 
+
+//Se declara un arreglo de caracteres para el nombre de los planetas y asi crear los archivos correspondientes
 char nombre[20];
+
+//Se declara una variable entera para dar el numero de planetas y por lo tanto el numero de ciclos que se haran
 int n;
 
+//Se declara una variable para leer el documento donde se encuentra la informacion del lugar en donde estan los planetas
 FILE *leer;
 leer=fopen("base.txt", "r");
-fscanf(leer, "%i", &n);
+fscanf(leer, "%i", &n); //Se lee el numero de planetas, el numero de veces que se hara el ciclo
 
 for (int i=0; i<n; i++){
-fscanf(leer, "%s", nombre);
-fscanf(leer, "%lf %lf %lf %lf %lf %lf", &x, &y, &z, &vx, &vy, &vz);
-fscanf(leer, "%lf %lf %lf", &me, &t, &h);
 
+fscanf(leer, "%s", nombre); 
+//Se lee el nombre del planeta y se le asigna la variable tipo char
+fscanf(leer, "%lf %lf %lf %lf %lf %lf", &x, &y, &z, &vx, &vy, &vz); 
+//Se leen las posiciones y velocidades iniciales
+fscanf(leer, "%lf %lf %lf %lf",&mp ,&me, &t, &h);
+//Se lee la masa del planeta, la masa de la estrella, el tiempo total, y el paso del tiempo
+
+//Se convierten las velocidades a años, ya que las velocidades se encuentran en dias 
 vx = vx * 365.2425;
 vy = vy * 365.2425;
 vz = vz * 365.2425;
 
+//Se declara una nueva variable tipo FILE, pero esta vez para escribir
 FILE *esc;
-esc = fopen(nombre, "w");
-fprintf(esc, "Posicion                     Velocidad\n");
-fprintf(esc, "t=%f, %f, %f, %f, %f, %f, %f\n",j, x, y, z, vx, vy, vz);
+esc = fopen(nombre, "w"); 
+//Se crea un archivo con el nombre del planeta, este proceso se realiza n veces, donde n es el numero de planetas, por lo tanto se crearan n archivos con los datos obtenidos por los calculos, y estos archivos tendran cada uno el nombre de sus respectivos planetas ya que se inicializan las variables para escribir, y por lo tanto los archivos seran diferentes 
 
-	for (j=0.0;j<t;j = j + h){
+//Se imprime las posiciones y velocidades iniciales
+fprintf(esc, "Posicion                     Velocidad\n");
+fprintf(esc, "%f, %f, %f, %f, %f, %f\n",x, y, z, vx, vy, vz);
+
+
+//Se realiza el metodo de Euler, que consiste en sumar al contador j, intervalos de tiempo h hasta llegar al tiempo total t, y mientras ir calculando los puntos
+
+	for (double j=0.0;j<t;j = j + h){
 	r = sqrt(pow(x,2)+pow(y,2)+pow(z,2));
 	x = x + vx * h;
 	y = y + vy * h;
@@ -34,13 +56,18 @@ fprintf(esc, "t=%f, %f, %f, %f, %f, %f, %f\n",j, x, y, z, vx, vy, vz);
 	vy = vy - h * ((4*3.14159265358*3.14159265358*y)/(pow(r, 3)));
 	vz = vz - h * ((4*3.14159265358*3.14159265358*z)/(pow(r, 3)));
 
-	fprintf(esc, "t=%f, %f, %f, %f, %f, %f, %f\n",j, x, y, z, vx, vy, vz);
+	//Se imprimen los puntos calculados y se reinicia el ciclo, para calcular nuevos puntos
+	fprintf(esc, "%f, %f, %f, %f, %f, %f\n", x, y, z, vx, vy, vz);
 
 	}
+
+//Se cierra la variable de escritura, para cuando se reinicie el ciclo, se abra una nueva variable y se escriba en un nuevo archivo
 fclose(esc);
 
 
 }
+
+//Se cierra la variable de lectura
 fclose(leer);
 
 return 0;
