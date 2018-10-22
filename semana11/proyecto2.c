9b01dabf5cd9e83425511ab58379a0313171b842
Segura Gomez Guillermo <seguragg2018@dci25-181.ugto.mx>
2018-10-22 11:51:43 -0500

    Proyecto

--- semana11/proyecto2.c
+++ semana11/proyecto2.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(){
 
-double **mat
+double **mat, a, b, c, d, ex, n;
+FILE *leer;
+leer = fopen("base.txt", "r");
+fscanf(leer, "%lf %lf %lf %lf %lf %lf", &a, &b, &c, &d, &ex, &n);
 
-mat = (double **) malloc(m  * sizeof(double*));
+mat[2][2]=5.0;
 
-for (int r=0;r<m;r++){
+mat = (double **) malloc(n  * sizeof(double*));
+for (int r=0;r<n;r++){
 	mat[r]= (double *) malloc(n * sizeof(double)); 
+	printf("%f", mat[r][r]);
 }
 
-
+fclose(leer);
 
 }
