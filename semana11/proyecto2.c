4fbabcbad92df831baa1bf6ef47f171441949c25
Segura Gomez Guillermo <seguragg2018@dci25-158.ugto.mx>
2018-10-22 09:52:15 -0500

    Proyecto

---
+++ semana11/proyecto2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+double **mat
+
+mat = (double **) malloc(m  * sizeof(double*));
+
+for (int r=0;r<m;r++){
+	mat[r]= (double *) malloc(n * sizeof(double)); 
+}
+
+
+
+}
