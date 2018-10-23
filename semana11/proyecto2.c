972bd5abe8cb46565963f06847790fe522e92ec1
seguragg2018 <seguragg2018@licifug.ugto.mx>
2018-10-23 03:43:11 +0000

    proyecto

--- semana11/proyecto2.c
+++ semana11/proyecto2.c
@@ -7,14 +7,23 @@ double **mat, a, b, c, d, ex, n;
 FILE *leer;
 leer = fopen("base.txt", "r");
 fscanf(leer, "%lf %lf %lf %lf %lf %lf", &a, &b, &c, &d, &ex, &n);
+printf("%f %f %f %f %f %f\n", a, b, c, d, ex, n);
 
-mat[2][2]=5.0;
 
 mat = (double **) malloc(n  * sizeof(double*));
 for (int r=0;r<n;r++){
 	mat[r]= (double *) malloc(n * sizeof(double)); 
-	printf("%f", mat[r][r]);
 }
+
+
+for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%f ", mat[i][j]);
+        }
+        printf("\n");
+    }
+
+
 
 fclose(leer);
 
