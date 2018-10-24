2cfc765d106e69dd4a9974c0be2247ab247e7f46
Segura Gomez Guillermo <seguragg2018@dci25-158.ugto.mx>
2018-10-24 09:53:18 -0500

    Proyecto

--- semana11/proyecto2.c
+++ semana11/proyecto2.c
@@ -3,7 +3,7 @@
 
 int main(){
 
-double **mat, a, b, c, d, ex, e=0, p=100;
+double **mat, a, b, c, d, ex, e=0, p=100, eps;
 int n, q=0;
 FILE *leer;
 leer = fopen("base.txt", "r");
@@ -21,7 +21,7 @@ for (int r=0;r<(n+2);r++){
 for (int i = 1; i<(n+1); i++){
 	for (int j=1; j<(n+1); j++){
 
-	scanf("%lf", &mat[i][j]);
+	mat[i][j]=0.0;
 
 }
 }
@@ -40,19 +40,31 @@ while(p>ex){
 for (int i = 1; i<(n+1); i++){
 	for (int j=1; j<(n+1); j++){
 
-	mat[i][j]= ((mat[i+1][j]+mat[i][j+1]+mat[i-1][j]+mat[i][j-1])/4);
+	mat[i][j]= ((mat[i+1][j]+mat[i][j+1]+mat[i-1][j]+mat[i][j-1])/4.0);
+
+//eps= (mat[1][1]-e)/mat[1][1];
+//e= mat[1][1];
 	
 }
 }
 
-e=(mat[1][1]-e)/mat[1][1];
-p = e*100;
+eps= (mat[1][1]-e)/mat[1][1];
+e= mat[1][1];
+
+//eps[i]=mat[1][1];
+//e = (mat[1][1]-eps[i-1])/mat[1][1];
+
+p = eps*100;
+
 printf("%f\n", p);
-q=q+1;
-if (q>10){
+
+if (q>50){ 
 printf("%f %i\n", p, q);
 break;
 }
+//p = e*100;
+q=q+1;
+
 
 
 }
@@ -67,6 +79,7 @@ for (int i = 0; i < (n+2); i++) {
 
 printf("\n%f %f\n", ex, p);
 
+free(mat);
 fclose(leer);
 
 }
