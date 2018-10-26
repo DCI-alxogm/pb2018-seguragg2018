d431a10548ab77224109a48af2407ec5c5798278
seguragg2018 <seguragg2018@licifug.ugto.mx>
2018-10-26 00:44:52 +0000

    Proyecto

--- semana11/proyecto2.c
+++ semana11/proyecto2.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 
 double **iniciar(double **m, int a);
-void **imp(double **m, int a);
+void **imp(double **m, int a, int q, double p, double ex);
 
 int main(){
 
@@ -21,6 +21,7 @@ for (int r=0;r<(n+2);r++){
 
 
 mat = iniciar(mat, n);
+
 //for (int i = 1; i<(n+1); i++){
 //	for (int j=1; j<(n+1); j++){
 
@@ -45,10 +46,13 @@ for (int i = 1; i<(n+1); i++){
 
 	mat[i][j]= ((mat[i+1][j]+mat[i][j+1]+mat[i-1][j]+mat[i][j-1])/4.0);
 
-imp
+
+
 	
 }
 }
+
+//imp(mat, n, q, p, ex);
 
 eps= (mat[1][1]-e)/mat[1][1];
 e= mat[1][1];
@@ -56,6 +60,7 @@ e= mat[1][1];
 
 p = eps*100;
 
+imp(mat, n, q, p, ex);
 
 q=q+1;
 if (q>500) break;
@@ -63,18 +68,20 @@ if (q>500) break;
 }
 
 
-for (int i = 1; i < (n+1); i++) {
+/*for (int i = 1; i < (n+1); i++) {
         for (int j = 1; j < (n+1); j++) {
             printf("%f ", mat[i][j]);
         }
         printf("\n");
     }
+*/
 
 printf("\n%f %f %i\n", ex, p, q);
 
 free(mat);
 fclose(leer);
 
+return 0;
 }
 
 double **iniciar(double **m, int a){
@@ -88,4 +95,27 @@ for (int i = 1; i<(a+1); i++){
 
 return m;
 
+}
+
+void **imp(double **m, int a, int q, double p, double ex){
+FILE *esc;
+
+if (p>ex){
+char arc[6]={'P','n','.','t','x','t'};
+arc[1]=q+'1';
+char *hivo;
+hivo = arc;
+esc = fopen(hivo, "w");
+}
+if (p<ex){
+esc = fopen("Equilibrio_final.txt", "w");
+}
+
+for (int i = 1; i < (a+1); i++) {
+        for (int j = 1; j < (a+1); j++) {
+            fprintf(esc, "%f ", m[i][j]);
+        }
+        fprintf(esc, "\n");
+    }
+fclose(esc);
 }
