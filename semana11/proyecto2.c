ce8d3121cecf03dff3cfdd226b2c1f82c31aa0da
seguragg2018 <seguragg2018@licifug.ugto.mx>
2018-10-27 00:12:14 +0000

    Po

--- semana11/proyecto2.c
+++ semana11/proyecto2.c
@@ -1,96 +1,87 @@
-#include<stdio.h>
+//Programa creado por Guillermo Segura Gomez
+//Ultima edicion 26/10/18
+
+//Se incluyen las librerias necesarias para el programa
+#include<stdio.h> 
 #include<stdlib.h>
 
+//Se declaran tres variables 
+//Una para inicializar la matriz en todos sus puntos. Los puntos seran iguales a 0.0
 double **iniciar(double **m, int a);
+
+//Otra para inicializar los bordes de la matriz con las temperaturas establecidas por el usuario
 double **lados(double **m, int n, double a, double b, double c, double d);
+
+//Y finalmente la ultima para imprimir la matriz en archivos diferentes para cada iteracion
 void **imp(double **m, int a, int q, double p, double ex);
 
+//Se utlizan dos matrices de tipo entrada y salida, porque los datos son escaneados e inicializados por el programa, con estos datos se llama a la funcion y la funcion arroja una matriz con los parametros requeridos
+//Se utiliza una funcion de tipo entrada y sin salida, ya que el programa no requiere que la funcion arroje nada, simplemente que imprima la matriz. Por lo que se le dan los valores que seran necesarios a la funcion, y la funcion imprime la matriz, y sigue la otra parte del programa.
+
+
+//Inicio de la funion main
 int main(){
 
-double **mat, a, b, c, d, ex, e=0, p=100, eps;
-int n, q=0;
+//Se declaran las variables asi como la matriz a usar en el  programa
+
+double **mat, a, b, c, d, ex, e=0, p=100, eps; //La matriz(**mat), los bordes de la placa(a, b, c, d), el error minimo(ex), el error calculado(p), y otras variables necesarias para realizar los calculos(eps, e)
+int n, q=0; //Los lados de la placa, y un contador 
+
+//Se declara una variable de lectura
 FILE *leer;
+
+//El archivo que se abre tiene como nombre "base.txt", en el se escanean los valores de los bordes de la placa (a, b, c, d), el error minimo(ex) y el numero de lados de la placa(n)
 leer = fopen("base.txt", "r");
 fscanf(leer, "%lf %lf %lf %lf %lf %i", &a, &b, &c, &d, &ex, &n);
 
+//Se cierra la variable de lectura
 fclose(leer);
 
+//Ya que la matiz se declaro como apuntador, se tiene que reservar la memoria, primero para las filas y luego para las columnas
+//La memoria se reserva de (n+2)*(n+2) en lugar de una de n*n, esto se hace con el fin de tener una matriz mucho dos columnas y dos filas mas grande para que en esos lugares se guarde la temperatura inicial de los bordes de la placa
 mat = (double **) malloc((n+2)  * sizeof(double*));
 for (int r=0;r<(n+2);r++){
 	mat[r]= (double *) malloc((n+2) * sizeof(double)); 
 }
 
-
+//En esta funcion se inicializa la matriz a 0.0 unicamente en la region en donde se va a trabajar, de 1,1 a (n-1),(n-1)
 mat = iniciar(mat, n);
 
-//for (int i = 1; i<(n+1); i++){
-//	for (int j=1; j<(n+1); j++){
-
-//	mat[i][j]=0.0;
-
-//}
-//}
-
-/*
-for (int i=0; i<(n+1);i++){
-	
-	mat[i][0]=a;
-	mat[0][i]=b;
-	mat[i][n+1]=c;
-	mat[n+1][i]=d;
-	
-}
-
-*/
-
-
+//En esta funcion se inicializan los bordes de la matriz con las temperaturas iniciales de la placa, como las temperaturas en los bordes siempre permanecen constantes, al momento de trabajar la matriz, esos puntos no se tocan por el programa, siempre son iguales
 mat = lados(mat, n, a, b, c, d);
 
 while(p>ex){
-//for (q=0; p>ex; q++){
-
-for (int i = 1; i<(n+1); i++){
-	for (int j=1; j<(n+1); j++){
-
-	mat[i][j]= ((mat[i+1][j]+mat[i][j+1]+mat[i-1][j]+mat[i][j-1])/4.0);
 
 
+	for (int i = 1; i<(n+1); i++){
+		for (int j=1; j<(n+1); j++){
 
-	
-}
-}
+			mat[i][j]= ((mat[i+1][j]+mat[i][j+1]+mat[i-1][j]+mat[i][j-1])/4.0);
 
-//imp(mat, n, q, p, ex);
-
-eps= (mat[1][1]-e)/mat[1][1];
-e= mat[1][1];
+		}
+	}
 
 
-p = eps*100;
+	eps= (mat[1][1]-e)/mat[1][1];
+	e= mat[1][1];
 
-if (q%10==0 || p<ex){
-imp(mat, n, q/10, p, ex);
-}
 
-q=q+1;
+	p = eps*100;
 
-if (q>500){
-printf("Error, numero de iteraciones exedidas %i\nEl error calculado fue de %f\n",q ,p);
-p = p-100.0;
-imp(mat, n, q/10, p, ex);
-return 0;
-}
+	if (q%10==0 || p<ex){
+		imp(mat, n, q/10, p, ex);
+	}
+
+	q=q+1;
+
+	if (q>500){
+		printf("Error, numero de iteraciones exedidas %i\nEl error calculado fue de %f\n",q ,p);
+		p = p-100.0;
+		imp(mat, n, q/10, p, ex);
+		return 0;
+	}
 
 }
-
-
-/*for (int i = 1; i < (n+1); i++) {
-        for (int j = 1; j < (n+1); j++) {
-            printf("%f ", mat[i][j]);
-        }
-        printf("\n");
-    }
-*/
 
 printf("Se hicieron un numero total de %i iteraciones\n", q);
 printf("El error fue de %f que es menor al establecido inicialmente %f\n", p, ex);
@@ -101,55 +92,3 @@ free(mat);
 return 0;
 }
 
-/*
-double **iniciar(double **m, int a){
-for (int i = 1; i<(a+1); i++){
-	for (int j=1; j<(a+1); j++){
-
-	m[i][j]=0.0;
-
-}
-}
-
-return m;
-
-}
-
-double **lados(double **m, int n, double a, double b, double c, double d){
-for (int i=0; i<(n+1);i++){
-	
-	m[i][0]=a;
-	m[0][i]=b;
-	m[i][n+1]=c;
-	m[n+1][i]=d;
-	
-}
-
-return m;
-
-}
-
-void **imp(double **m, int a, int q, double p, double ex){
-FILE *esc;
-
-if (p>ex){
-char arc[6]={'P','n','.','t','x','t'};
-arc[1]=q+'A';
-char *hivo;
-hivo = arc;
-esc = fopen(hivo, "w");
-}
-if (p<ex){
-esc = fopen("Equilibrio_final.txt", "w");
-}
-
-
-for (int i = 1; i < (a+1); i++) {
-        for (int j = 1; j < (a+1); j++) {
-            fprintf(esc, "%f ", m[i][j]);
-        }
-        fprintf(esc, "\n");
-    }
-fclose(esc);
-}
-*/
